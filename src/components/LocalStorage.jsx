3491efbea7d9fb0e1605697c8871ce5eed925933
Navin Ayer <ayernavin11@gmail.com>
2026-01-08 20:49:51 +0545

    Few Changes Made

--- src/components/LocalStorage.jsx
+++ src/components/LocalStorage.jsx
@@ -1,12 +1,11 @@
-const expenseTracker = "ExpenseTrackerApp"
-
+const expenseTracker = "ExpenseTracker"
 
 export const localStorageSetItem = (expenses)=>{
-        localStorage.setItem(expenseTracker, JSON.stringify(expenses))
+        return localStorage.setItem(expenseTracker, JSON.stringify(expenses))
 }
 
-export const localStorageGetItem = () =>{
- const data = localStorage.getItem(expenseTracker)
- if(!data) return []
- return JSON.parse(data)
+export const localStorageGetItem = ()=>{
+        const data = localStorage.getItem(expenseTracker)
+        if(!data) return []
+        return JSON.parse(data)
 }
